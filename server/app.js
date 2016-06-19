0860bd3ea67f479e51f30076521415fc8dfe157d
Alexey Velikiy <gmpota@gmail.com>
2016-06-19 03:14:11 +0300

    9-query-route-on-server

--- server/app.js
+++ server/app.js
@@ -10,3 +10,23 @@ app.use(express.static(__dirname + '/public'));
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+app.get('/frame/:id(\\d+)', function (req, res) {
+  var n = +req.params['id'];
+  res.setHeader('Content-Type', 'application/json');
+  res.sendFile(__dirname + '/public/index.html');
+
+  res.send(
+    JSON.stringify(computeGeoms(n))
+  );
+});
+
+
+function computeGeoms(n) {
+  return {
+    77: [1, 2, 3],
+    78: [4, 1, 1]
+  }
+}
+
+
--- server/public/app.js
+++ server/public/app.js
@@ -1,8 +1,32 @@
 var App = {}
 
+var ctx = undefined;
+
+App.N = 0;
+
 App.init = function() {
-  App.$canvas = $('#canvas');
-  alert(App.$canvas.attr('width'));
+  App.N = 0;
+  ctx = document.getElementById("canvas").getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,150,75);
+  App.initNextFrameRequest();
+}
+
+App.initNextFrameRequest = function() {
+  $.get('/frame/' + App.N, function(data) {
+    if(data.end) {
+      // TODO: special effect for this
+      return;
+    }
+    App.onNextFrame(data);
+    App.N++;
+    App.initNextFrameRequest();
+  });
+}
+
+App.onNextFrame = function(frameData) {
+  // TODO: process data
+  console.log(frameData);
 }
 
 
