8e62f60c085decde657e41a4d2f7acde1e4b5660
Alexey Velikiy <gmpota@gmail.com>
2016-06-19 14:15:47 +0300

    building circles

--- server/public/app.js
+++ server/public/app.js
@@ -6,10 +6,10 @@ var canvas;
 App.N = 0;
 
 var REGIONS = {
-  77: { color: '#1f77b4', center:[50, 50] },
+  77: { color: '#d62728', center:[50, 50] },
   78: { color: '#ff7f0e', center:[20, 20] },
   54: { color: '#2ca02c', center:[80, 20] },
-  66: { color: '#d62728', center:[20, 80] },
+  66: { color: '#1f77b4', center:[20, 80] },
   52: { color: '#9467bd', center:[80, 80] }
 }
 
@@ -19,6 +19,15 @@ function x(f) {
 
 function y(f) {
   return (f / 100) * canvas.height;
+}
+
+function rotate(cx, cy, x, y, angle) {
+    var radians = (Math.PI / 180) * angle,
+        cos = Math.cos(radians),
+        sin = Math.sin(radians),
+        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+    return [nx, ny];
 }
 
 App.init = function() {
@@ -54,12 +63,22 @@ App.onNextFrame = function(frameData) {
 }
 
 App.drawStreet = function(cityId) {
-  function r() {
-    return Math.random() * 14 - 7;
-  }
+  
   ctx.beginPath();
-  var pa = [REGIONS[cityId].center[0] + r(), REGIONS[cityId].center[1] + r()];
-  var pb = [REGIONS[cityId].center[0], REGIONS[cityId].center[1]];
+  
+  var length = Math.random() * 25;
+  
+  var pa = rotate(
+    REGIONS[cityId].center[0], REGIONS[cityId].center[1], 
+    REGIONS[cityId].center[0] + length, REGIONS[cityId].center[1],
+    360 * Math.random()
+  );
+  
+  length = Math.random() * 2;
+  
+  var pb = rotate(
+    pa[0], pa[1], pa[0] + length, pa[1], 360 * Math.random()
+  );
   ctx.moveTo(x(pa[0]), y(pa[1]));
   ctx.lineTo(x(pb[0]), y(pb[1]));
   ctx.closePath();
@@ -68,17 +87,17 @@ App.drawStreet = function(cityId) {
 }
 
 App.drawBuilding = function(cityId) {
-  function r() {
-    return Math.random() * 20 - 10;
-  }
-  // TODO: use size
+  length = Math.random() * 25;
+  var pa = rotate(
+    REGIONS[cityId].center[0], REGIONS[cityId].center[1], 
+    REGIONS[cityId].center[0] + length, REGIONS[cityId].center[1],
+    360 * Math.random()
+  );
   ctx.beginPath();
-  var pa = [REGIONS[cityId].center[0] + r(), REGIONS[cityId].center[1] + r()];
-  var pb = [pa[0] + 1, pa[1] + 1];
-  ctx.moveTo(x(pa[0]), y(pa[1]));
-  ctx.lineTo(x(pb[0]), y(pb[1]));
-  ctx.closePath();
-  ctx.strokeStyle = REGIONS[cityId].color
+  ctx.beginPath();
+  ctx.arc(x(pa[0]), y(pa[1]), 3, 0, 2 * Math.PI, false);
+  ctx.lineWidth = 0.3;
+  ctx.strokeStyle = REGIONS[cityId].color;
   ctx.stroke();
 }
 
